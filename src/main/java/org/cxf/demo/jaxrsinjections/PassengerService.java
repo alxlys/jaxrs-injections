e076f5ebdc7e1bc8d96d4463e4870fd447d1332d
Oleksandr Lysenko <olysenko@vmware.com>
2021-08-14 11:04:39 +0300

    request headers

--- src/main/java/org/cxf/demo/jaxrsinjections/PassengerService.java
+++ src/main/java/org/cxf/demo/jaxrsinjections/PassengerService.java
@@ -3,22 +3,34 @@ package org.cxf.demo.jaxrsinjections;
 import org.cxf.demo.jaxrsinjections.model.Passenger;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import java.util.List;
 
 @Path("/passengerservice")
 @Produces("application/xml")
-@Consumes("application/xml")
+@Consumes("application/xml,application/x-www-form-urlencoded")
 public interface PassengerService {
 
    @GET
    @Path("/passengers")
-   List<Passenger> getPassengers();
+   List<Passenger> getPassengers(@QueryParam("start") int start, @QueryParam("size") int size);
 
    @POST
    @Path("/passengers")
    Passenger addPassenger(Passenger passenger);
+
+   @POST
+   @Path("/passengers")
+   void addPassenger(@FormParam("firstName") String firstName,
+         @FormParam("lastName") String lastName, @HeaderParam("agent") String agent,
+         @Context HttpHeaders httpHeaders);
+
 }
--- src/main/java/org/cxf/demo/jaxrsinjections/PassengerServiceImpl.java
+++ src/main/java/org/cxf/demo/jaxrsinjections/PassengerServiceImpl.java
@@ -3,6 +3,8 @@ package org.cxf.demo.jaxrsinjections;
 import org.cxf.demo.jaxrsinjections.model.Passenger;
 import org.springframework.stereotype.Service;
 
+import javax.ws.rs.core.HttpHeaders;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,11 +13,12 @@ import java.util.List;
 @Service
 public class PassengerServiceImpl implements PassengerService {
 
-   private List<Passenger> passengers;
+   private final List<Passenger> passengers = new ArrayList<>();
    private int currentId = 1;
 
    @Override
-   public List<Passenger> getPassengers() {
+   public List<Passenger> getPassengers(int start, int size) {
+      System.out.println("start = " + start + ", size = " + size);
       return passengers;
    }
 
@@ -24,5 +27,21 @@ public class PassengerServiceImpl implements PassengerService {
       passenger.setId(currentId++);
       passengers.add(passenger);
       return passenger;
+   }
+
+   @Override
+   public void addPassenger(String firstName, String lastName, String agent,
+         HttpHeaders httpHeaders) {
+      System.out.println("Agent: " + agent);
+      httpHeaders.getRequestHeaders().keySet().forEach(k -> System.out
+            .println("headerName: " + k + ", value: " + httpHeaders.getHeaderString(k)));
+      System.out.println("***************************************************************");
+      httpHeaders.getCookies().keySet().forEach(k -> System.out.println(
+            "cookieName: " + k + ", value: " + httpHeaders.getCookies().get(k).getValue()));
+
+      Passenger passenger = new Passenger();
+      passenger.setId(currentId++);
+      passenger.setName(firstName + " " + lastName);
+      passengers.add(passenger);
    }
 }
